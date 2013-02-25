fb320d0e3439a9f196db2373ba9b06b517bec377
David Peek <david.peek@me.com>
2013-02-26 00:00:37 +1100

    Moved static files to /theme. Added post generation, the pattern '\n\n--\n\n' is used to indicate summary/ fulltext. Path handling needs work. Site is now properly generated to deploy. Added welcome post.

--- build.dart
+++ build.dart
@@ -1,28 +1,53 @@
-import "dart:io";
+import 'dart:io';
 
 import 'package:markdown/markdown.dart' show markdownToHtml;
 import 'package:html5lib/parser.dart';
 import 'package:yaml/yaml.dart';
 import 'package:intl/intl.dart';
 
+var deploy = new Directory('deploy');
+var theme = new Directory('theme');
+var post = new Directory('post');
+
+var title = "Dartful";
+
 void main() {
+  prepareDeploy();
   var files = getMarkdownFiles();
   var posts = files.map(readPost).toList();
   writeIndex(posts);
   posts.forEach(writePost);
 }
 
+/// Prepare the deploy directory.
+void prepareDeploy() {
+  if (!deploy.existsSync()) deploy.createSync();
+  theme.list().onFile = (f) {
+    var path = new Path(f);
+    copyFile(f, '${deploy.path}/${path.filename}');
+  };
+}
+
+/// Copy a file from [input] to [output]
+void copyFile(String input, String output) {
+  final inStream = new File(input).openInputStream();
+  final outStream = new File(output).openOutputStream(FileMode.WRITE);
+  inStream.pipe(outStream);
+}
+
 /// Get a list of all markdown files in the site.
 List<File> getMarkdownFiles() {
-  var entries = new Directory("post").listSync();
+  if (!post.existsSync()) return [];
+  var entries = post.listSync();
   entries.retainMatching((entry) {
     return entry is File 
-        && new Path(entry.fullPathSync()).extension == "markdown";
+        && new Path(entry.fullPathSync()).extension == 'markdown';
   });
   return entries;
 }
 
 var _RE_FRONT = new RegExp(r'---\n((.+\n)+)---', multiLine:true);
+var _RE_PREVIEW = new RegExp(r'\n\n--\n\n', multiLine:true);
 
 /// Reads a post file from disk and returns a Post model.
 Post readPost(File file) {
@@ -35,47 +60,64 @@ Post readPost(File file) {
     loadYaml(match[1]).forEach((k, v) => front[k] = v);
     content = content.substring(match.end).trim();
   }
-  return new Post(front['title'], content, DateTime.parse(front['date']));
+  
+  var preview = content;
+  var full = content;
+  
+  var index = content.indexOf('\n\n--\n\n');
+  if (index > -1) {
+    preview = content.substring(0, index);
+    full = content.split('\n\n--\n\n').join("\n\n");
+  }
+  
+  var pwd = new Directory.current();
+  var path = file.fullPathSync().substring(pwd.path.length + post.path.length + 2);
+  var parts = path.split('-');
+  var dir = parts.getRange(0, 3).join("/");
+  var name = parts.getRange(3, parts.length - 3).join("-");
+  name = name.replaceFirst('.markdown', '.html');
+  path = '$dir/$name';
+  return new Post(path, front['title'], preview, full, DateTime.parse(front['date']));
 }
 
 /// Writes the sites index pags, containing all posts.
 void writeIndex(List<Post> posts) {
-  var content = '';
-  posts.reversed.forEach((post) {
-    content = '$content${post.html}';
-  });
-  var html = createHtml(content);
-  new File('output/index.html').writeAsString(html, mode:FileMode.WRITE, encoding:Encoding.UTF_8);
+  new File('${deploy.path}/index.html').writeAsString(indexTemplate(posts), 
+      mode:FileMode.WRITE, encoding:Encoding.UTF_8);
 }
 
 // Writes and individual post page.
 void writePost(Post post) {
-  // write post
+  var path = new Path('${deploy.path}/${post.path}');
+  
+  var dir = new Directory.fromPath(path.directoryPath);
+  if (!dir.existsSync()) dir.createSync(recursive:true);
+  
+  new File.fromPath(path).writeAsString(postTemplate(post), 
+      mode:FileMode.WRITE, encoding:Encoding.UTF_8);
 }
 
 /// The model for a post.
 class Post {
+  String path;
   String title;
+  String preview;
   String content;
   DateTime date;
   
-  Post(this.title, this.content, this.date);
+  Post(this.path, this.title, this.preview, this.content, this.date);
   
   String get dateString {
-    return new DateFormat("EEEE, d MMMM y", "en_US").format(date);
+    return new DateFormat('EEEE, d MMMM y', 'en_US').format(date);
   }
-  
-  String get html => '<article>$headerHtml$contentHtml</article>';
-  String get headerHtml => '<header><h1>$title</h1><span>$dateString</span></header>';
-  String get contentHtml => '<div>${markdownToHtml(content)}</div>';
 }
 
-/// An HTML helper until we sort out templates.
-String createHtml(String content) {
-  return
+// templates
+
+String indexTemplate(List<Post> posts) { return
 '''<html>
 <head>
-  <title>Dartful</title>
+  <title>$title</title>
   <link href='styles.css' rel='stylesheet' type='text/css'>
   <link rel="shortcut icon" href="favicon.ico">
 </head>
@@ -84,7 +126,41 @@ String createHtml(String content) {
     <header id='banner'>
       <h1>Dartful</h1>
     </header>
-    $content
+    ${posts.map(postPartial).join('')}
+  </div>
+</body>
+</html>''';
+}
+
+String postPartial(Post post) { return
+'''<article>
+  <header>
+    <a href="${post.path}"><h1>${post.title}</h1></a>
+    <span>${post.dateString}</span>
+  </header>
+  <div>${markdownToHtml(post.preview)}</div>
+</article>''';
+}
+
+String postTemplate(Post post) { return
+'''<html>
+<head>
+  <title>$title - ${post.title}</title>
+  <link href='../../../styles.css' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="favicon.ico">
+</head>
+<body>
+  <div id='content'>
+    <header id='banner'>
+      <h1>Dartful</h1>
+    </header>
+    <article>
+      <header>
+        <h1>${post.title}</h1>
+        <span>${post.dateString}</span>
+      </header>
+      <div>${markdownToHtml(post.content)}</div>
+    </article>
   </div>
 </body>
 </html>''';
---
+++ post/2013-02-25-welcome-to-dart-blog.markdown
@@ -0,0 +1,12 @@
+---
+title: Welcome to Dart Blog
+date: 2013-02-25 20:12
+---
+
+To get started, add markdown posts to the `/post` directory and run 
+`dart build.dart` from the command line to generate. Your blog will be 
+generated in the `/deploy` directory.
+
+--
+
+The full post appears after the `--` marker.
